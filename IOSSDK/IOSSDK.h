d32f4f559186bbe50ebd790a4c3c6e46d046b41e
LogeshM05 <logeshwarlogs05@gmail.com>
2024-03-18 12:48:49 +0530

    Initial Commit

---
+++ IOSSDK/IOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  IOSSDK.h
+//  IOSSDK
+//
+//  Created by Logeshwar M on 18/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOSSDK.
+FOUNDATION_EXPORT double IOSSDKVersionNumber;
+
+//! Project version string for IOSSDK.
+FOUNDATION_EXPORT const unsigned char IOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSSDK/PublicHeader.h>
+
+
---
+++ IOSSDK/IOSSDK.swift
@@ -0,0 +1,19 @@
+//
+//  IOSSDK.swift
+//  IOSSDK
+//
+//  Created by Logeshwar M on 18/03/24.
+//
+
+import Foundation
+
+public class IOSSDK {
+    
+    
+    public static func getVersion() -> String {
+        
+        return "iOS framework version - 1.0.0"
+    }
+    
+    
+}
