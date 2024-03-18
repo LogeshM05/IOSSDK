da2aa60e8aac7c45c6e160d7c1b75a2804518970
LogeshM05 <logeshwarlogs05@gmail.com>
2024-03-18 14:09:08 +0530

    second version commit

--- IOSSDK/IOSSDK.swift
+++ IOSSDK/IOSSDK.swift
@@ -12,8 +12,9 @@ public class IOSSDK {
     
     public static func getVersion() -> String {
         
-        return "iOS framework version - 1.0.0"
+        return "iOS framework version - 2.0.0"
     }
+    
     
     
 }
